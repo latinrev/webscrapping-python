15affb9507483583e87881529eb28fc9cd004e28
Joel Castillo <46919967+latinrev@users.noreply.github.com>
2019-10-07 17:01:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,4 @@ for link in all_links:
                 |                    DOLAR TODAY PRICE                  |
                 |                                                       |
                 |///////////////////////////////////////////////////////|""")
-print(price);        
-Simple script using Web scrapping to get the price US Dollars in Bolivares Soberanos
-# webscrapping-python
-Simple script using Web scrapping get the price US Dollars in Bolivares Soberanos
-#How does it work?
-It uses python library Beautiful Soup 4(Compatible with python 3+) which it uses to get an image from the website.
-> An image? But you are displaying text in the console!
-Well basically we use another python library called PyTesseract, which uses the OCR called tesseract which we will use to convert the image we downloaded to text and display it as a text
+print(price);
